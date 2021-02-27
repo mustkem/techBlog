dfd0dd1056f6dacda846f7bea96f5e2bf2e99717
mustkem <mo.mustkem@gmail.com>
2021-02-27 14:39:28 +0530

    add

--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
-// export const API_URL = "https://woodenculture-backend.herokuapp.com";
-export const API_URL = "http://localhost:8080";
+export const API_URL = "https://codemedium-backend.herokuapp.com";
+// export const API_URL = "http://localhost:8080";
+
