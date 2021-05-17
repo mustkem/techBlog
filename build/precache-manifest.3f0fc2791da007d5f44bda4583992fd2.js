249e0bb8dc347cc29dde9c88d0b810fe74565dd5
mustkem <mo.mustkem@gmail.com>
2021-05-17 15:46:43 +0530

    deploy firebase version of app

---
+++ build/precache-manifest.3f0fc2791da007d5f44bda4583992fd2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "194f3f84458f01c6d2407f8136354668",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4926f95fee687043e981",
+    "url": "/static/css/2.6262c566.chunk.css"
+  },
+  {
+    "revision": "9cc63e3088222e20ee52",
+    "url": "/static/css/main.c6204df4.chunk.css"
+  },
+  {
+    "revision": "4926f95fee687043e981",
+    "url": "/static/js/2.9febf307.chunk.js"
+  },
+  {
+    "revision": "9cc63e3088222e20ee52",
+    "url": "/static/js/main.95bd3841.chunk.js"
+  },
+  {
+    "revision": "883b6f8d65d12df337d8",
+    "url": "/static/js/runtime~main.25354edc.js"
+  }
+]);
